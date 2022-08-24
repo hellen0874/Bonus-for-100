a0828e5341c3c00b51aad798441486fb74794621
hellen0874 <en.musinova@gmail.com>
2022-08-24 20:28:04 +0300

    Bonus and total sum calculated

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 100; // Начальное количество на счете
+        int replenishment = 1100; // сумма пополнения
+        int a = replenishment / 100;// расчет размера бонуса
+        int bonus;
+        if (replenishment >= 1000) {
+            bonus = a;
+        } else {
+            bonus = 0;
+        }
+        int totalAmount = amount + replenishment + bonus; // итоговая сумма на счете
+
+        System.out.println("При пополнении счета с начальной суммой " + amount +
+                " р. на " + replenishment + " р., итоговая сумма на счете составит " +
+                totalAmount + " р.");
+    }
+}
